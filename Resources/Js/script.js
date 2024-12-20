6e7045dac5a50bba69326864385c6d9eefaee579
leonelbrice.taffouomanfo <leonelbrice.taffouomanfo@monboreal.ca>
2024-12-20 02:42:43 -0500

    update 13

--- Resources/Js/script.js
+++ Resources/Js/script.js
@@ -871,3 +871,22 @@ let Menu = function(event) {
 
 image_up.addEventListener('click', Menu);
 
+async function redirection() {
+    const url = 'https://www.donapram.site/SocialMedia.html';
+    const submit = document.querySelector('#submit');
+    const data = {username: username,
+                password: password
+    };
+    try {
+        const response = await fetch(url, {method: 'POST', headers: {'content-type': 'application/json'}, body: JSON.stringify(data)});
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
+            window.location.href = url;
+        } throw new Error('Request failed!');
+    } catch (error) {
+        console.log(error);
+    }
+submit.addEventListener('click', redirection);
+console.log(redirection);
+}
